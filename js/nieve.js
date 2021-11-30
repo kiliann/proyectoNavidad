b07d24604dae2860f1878f7a098e52d5b2c41dc8
kiliann <wilian_kilian@hotmail.com>
2021-11-30 14:20:06 +0100

    Averia de nieve

--- js/nieve.js
+++ js/nieve.js
@@ -1,68 +1,82 @@
 'use strict'
 
- //window.onload= iniCopos(100,80);
+
     class oCopo{
         constructor(tam, id){
         this.x = 0;
         this.y = 0;
         this.size = tam;
         this.nombre = id
+        this.obj = null;
+        window.onload = this.iniciar.bind(this);
+    }
+    iniciar(){
         this.obj = document.createElement("div");
-        this.obj.setAttribute('id',id);
+        this.obj.setAttribute('id',this.nombre);
         this.obj.innerText="*";
         this.obj.style.position = "absolute";
-        this.obj.style.fontSize = tam+"px";
+        this.obj.style.fontSize = this.size+"px";
         this.obj.style.color = "white";
         document.body.appendChild(this.obj)
+        this.iniNevada();
     }
         dibujar(x,y){
         this.x = x;
         this.y = y;
+
         this.obj.style.top = this.y+"px";
         this.obj.style.left = this.x+"px";
     }
-    }
-        function iniCopos(num, anc, alto){
+
+        iniCopos(num, anc, alto){
             var copos = new Array(num);
             var tam, x, y;
-            for (let i = 0; i<num; i++)
-        {
-            tam = Math.round(Math.random()*10)+ 8;
-            copos[i] = new oCopo(tam, "c"+i);
-            x = parseInt(Math.random()*anc);
-            y = parseInt(Math.random()*alto);
-            copos[i].dibujar(x,y);
-        }
+            for (let i = 0; i<this.num; i++)  {
+                tam = Math.round(Math.random()*10)+ 8;
+                copos[i] = new oCopo(tam, "c"+i);
+                x = parseInt(Math.random()*anc);
+                y = parseInt(Math.random()*alto);
+                copos[i].dibujar(x,y);
+            }
             return copos;
         }
-        function iniNevada(num, vel)
+
+        iniNevada(num, vel)
         {
             var ancho = document.body.offsetWidth-10;
             var alto =  document.documentElement.scrollHeight;
-            var losCopos = iniCopos(num, ancho, alto)
-            nevar(losCopos, ancho,alto, vel);
+            var losCopos = this.iniCopos(this.num, ancho, alto)
+            this.nevar(losCopos, ancho,alto, vel);
         }
-        function nevar(copos, coposAncho, coposAlto, vel)
-        {
-            var x, y;
-            for (let i = 0; i < copos.length; i++)
-        {
-            y = copos[i].y;
-            x = copos[i].x;
-            if (Math.random() > 0.5)
-            y += parseInt(Math.random()+1);
-            y += parseInt(Math.random()+2);
-            if (y >= (coposAlto - copos[i].size))
-        {
-            y = Math.round(Math.random()*10);
-            x  =parseInt(Math.random()*coposAncho-1);
-        }
-            copos[i].dibujar(x,y);
-        }
+
+        nevar(copos, coposAncho, coposAlto, vel){
+                    var x, y;
+            for (let i = 0; i < this.copos.length; i++){
+
+                y = this.copos[i].y;
+                x = this.copos[i].x;
+                if (Math.random() > 0.5)
+                    y += parseInt(Math.random()+1);
+                y += parseInt(Math.random()+2);
+                if (y >= (coposAlto - copos[i].size))
+                {
+                    y = Math.round(Math.random()*10);
+                    x  =parseInt(Math.random()*coposAncho-1);
+                }
+                this.copos[i].dibujar(x,y);
+            }
             setTimeout(nevar, vel, copos,  coposAncho, coposAlto, vel);
         }
 
-let ejecutar = new oCopo(400, 100);
+    }
+
+
+
+
+
+
+
+let ejecutar = new oCopo(400, 200);
 
 
 
